5a39b1478bbf5307ea0774247898170b31842694
Sami <a1500928@myy.haaga-helia.fi>
2016-09-13 12:19:19 +0300

    kesken pitää viimeistellä

---
+++ src/main/java/com/tunnit/TuntiServlet.java
@@ -0,0 +1,55 @@
+package com.tunnit;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.beans.Tunnit;
+
+/**
+ * Servlet implementation class TuntiServlet
+ */
+@WebServlet("/TuntiServlet")
+public class TuntiServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public TuntiServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String tunnit = request.getParameter("tunnit");
+		String nimi = request.getParameter("nimi");
+		String kuvaus = request.getParameter("kuvaus");
+		String henkilo_id = request.getParameter("henkilo_id");
+		
+		Tunnit tunnin_tiedot = new Tunnit();
+		tunnin_tiedot.setId(Integer.parseInt(henkilo_id));
+		tunnin_tiedot.setKuvaus(kuvaus);
+		tunnin_tiedot.setTunnit(Integer.parseInt(tunnit));
+		
+		lisaaTunnitKantaan(tunnin_tiedot);
+		
+		response.sendRedirect("index.jsp");
+		
+	}
+
+}
