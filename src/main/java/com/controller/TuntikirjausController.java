08b433b8471dcae29e2416bafff348467dfb5260
Heikki Petrell <a1402763@myy.haaga-helia.fi>
2016-09-29 11:48:39 +0300

    Formin luominen MVC malliin ja uuden tunnin tallennus

--- src/main/java/com/controller/TuntikirjausController.java
+++ src/main/java/com/controller/TuntikirjausController.java
@@ -6,14 +6,14 @@ import javax.inject.Inject;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.beans.Henkilot;
 import com.beans.HenkilotImpl;
+import com.beans.Tunnit;
 import com.dao.TuntiDAO;
-
-
 
 
 @Controller
@@ -31,14 +31,26 @@ public class TuntikirjausController {
 		this.dao = dao;
 	}
 	
-	//Tuntien listaus
+	//Tuntien listaus ja formi
 	
 	@RequestMapping(value="/", method=RequestMethod.GET)
 	public String getView(Model model){
 		List<HenkilotImpl> henkilot = dao.haeTunnit();
 		model.addAttribute("henkilot", henkilot);
+		HenkilotImpl tyhjaHenkilo = new HenkilotImpl();
+		Tunnit  tyhjatTunnit = new Tunnit();
+		model.addAttribute("henkilo", tyhjaHenkilo);
+		model.addAttribute("tunnit", tyhjatTunnit);
+		System.out.println("halojaa");
 		return "index";
 	}
+	// Käyttäjän tuntien lisäys
+	@RequestMapping(value="/", method=RequestMethod.POST)
+	public String create( @ModelAttribute(value="henkilo") HenkilotImpl henkilot){
+		dao.talleta(henkilot);
+		return "redirect:/";
+	}
+	
 	
 //	@RequestMapping(value="/", method=RequestMethod.GET)
 //	public String getCreateForm(Model model) {	
