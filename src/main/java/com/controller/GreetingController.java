5728f5ec7cd8ac226c8107da7e300cd834cde5d9
Sami Nummela <sami.nummela@gmail.com>
2016-10-27 11:30:35 +0300

    java mainin kautta toimiva spring boot versio

--- src/main/java/com/controller/GreetingController.java
+++ src/main/java/com/controller/GreetingController.java
@@ -1,4 +1,4 @@
-package hello;
+package com.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,12 +7,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@RequestMapping (value="/")
+
 public class GreetingController {
 
-    @RequestMapping("/greeting")
+    @RequestMapping()
     public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+    	System.out.println("------ TOIIIIMIIIIKOOOO ?????   sadjfkladsjfladskfjadsklfö");
         model.addAttribute("name", name);
         return "greeting";
     }
-
 }
---
+++ src/main/java/com/controller/TuntikirjanpitoApplication.java
@@ -0,0 +1,15 @@
+package com.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class TuntikirjanpitoApplication {
+	
+    public static void main(String[] args) {
+        SpringApplication.run(TuntikirjanpitoApplication.class, args);
+    }
+
+}
