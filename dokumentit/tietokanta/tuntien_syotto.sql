4023f2d8aa82c3fa6db6e2360183381af442dd5a
Liljelund Aaro <a1501110@myy.haaga-helia.fi>
2016-11-17 11:29:41 +0200

    insertlause lisätty multiprojektitukea varten

--- dokumentit/tietokanta/tuntien_syotto.sql
+++ dokumentit/tietokanta/tuntien_syotto.sql
@@ -1,2 +1,4 @@
 INSERT INTO Tunnit(tuntien_maara, kuvaus, kayttaja_id)VALUES(2, 'suunnittelua', 1);
 INSERT INTO Tunnit (tuntien_maara, kuvaus, kayttaja_id) VALUES(?,?,?);
+----Multiprojektituen jälkeen syöttö-------
+INSERT INTO Tunnit(tuntien_maara, kuvaus, kayttaja_id, projekti)VALUES(2, 'suunnittelua', 1, 1);
