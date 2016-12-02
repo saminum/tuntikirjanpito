c261baf9a3e13dff8993dbc7829fc47b1793d755
aaroppi <aaroppi@live.com>
2016-12-02 16:33:25 +0200

    Kyhäily jatkuu

---
+++ src/main/java/com/beans/Unohdus.java
@@ -0,0 +1,19 @@
+package com.beans;
+
+public interface Unohdus {
+
+	public String getTunnus();
+
+	public void setTunnus(String tunnus);
+
+	public String getEmail();
+
+	public void setEmail(String email);
+
+	public String getSalasana();
+
+	public void setSalasana(String salasana);
+
+	public String toString();
+
+}
